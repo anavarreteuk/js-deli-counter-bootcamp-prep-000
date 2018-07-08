ca736561d4956bb4eebf5bd1b1aa90927574be4c
Adrian Navarrete <a.navarrete.uk@gmail.com>
2018-07-08 23:09:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,26 +3,26 @@
 function takeANumber(katzDeliLine, newname) {
 var katzDeli = [];
 katzDeliLine.push(newname);
-return `Welcome, ${newname}. You are number ${katzDeliLine.length} in line.`
+return `Welcome, ${newname}. You are number ${katzDeliLine.length} in line.`;
 }
 
 function nowServing(katzDeliLine) {
-  var katzDeli = []
+  var katzDeli = [];
   if(katzDeliLine.length === 0) {
-  return "There is nobody waiting to be served!"
+  return "There is nobody waiting to be served!";
 }
-return `Currently serving ${katzDeliLine.shift()}.`
+return `Currently serving ${katzDeliLine.shift()}.`;
 }
 
 function currentLine(katzDeliLine) {
   if(katzDeliLine.length === 0) {
-    return "The line is currently empty."
+    return "The line is currently empty.";
   }
-var numbersAndNames = []
+var numbersAndNames = [];
 
   for (let i = 0, l = katzDeliLine.length; i < l; i++) {
-    numbersAndNames.push(`${i + 1}. ${katzDeliLine[i]}`)
+    numbersAndNames.push(`${i + 1}. ${katzDeliLine[i]}`);
   }
 
-  return `The line is currently: ${numbersAndNames.join(', ')}`
+  return `The line is currently: ${numbersAndNames.join(', ')}`;
 }
